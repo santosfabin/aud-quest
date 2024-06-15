c76d1db1da7c87e2ac9c64fcf7f1b9caba9b34ed
santosfabin <fsfabianosantos03@gmail.com>
2024-06-15 10:49:35 -0300

    fixing theme

--- scripts/signup.js
+++ scripts/signup.js
@@ -55,23 +55,3 @@ form.addEventListener('submit', async function(event) {
 }); */
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    function getLocalStorage(key) {
-      return localStorage.getItem(key);
-    }
-  
-    function toggleLocalStorage(key, value) {
-      if (getLocalStorage(key)) {
-        return localStorage.removeItem(key);
-      }
-      return localStorage.setItem(key, value);
-    }
-  
-    document.querySelector('header nav span').addEventListener('click', () => {
-      document.body.classList.toggle('light');
-      toggleLocalStorage('theme', true);
-    });
-    if (getLocalStorage('theme')) {
-      document.body.classList.toggle('light');
-    }
-  });
