69f604a6f5bdcb45ba89bcd046ecf6651fe14392
santosfabin <fsfabianosantos03@gmail.com>
2024-06-15 08:46:47 -0300

    try

--- scripts/login.js
+++ scripts/login.js
@@ -20,8 +20,14 @@ document.getElementById('myForm').addEventListener('submit', async (event) => {
         if (response.ok) {
             // Armazena o token no localStorage (ou cookie, conforme preferir)
             localStorage.setItem('token', data.token);
-            responseDiv.textContent = 'Login bem-sucedido!';
-            // Redireciona ou faz outra ação após login bem-sucedido
+            responseDiv.textContent = 'Login bem-sucedido! Redirecionando...';
+
+            // Chama a função para buscar dados do usuário após o login bem-sucedido
+            await fetchUserData();
+
+            // Redirecionar para a página após login bem-sucedido
+            window.location.replace('/index.html'); // Altere para a página desejada após login
+
         } else {
             responseDiv.textContent = `Erro: ${data.error}`;
         }
